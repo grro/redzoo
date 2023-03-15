614b116f82e3af875a8dabdd7144c0ca7bb4d4be
grro <>
2023-03-15 13:53:47 +0100

    duration formatting fixed

--- redzoo/math/display.py
+++ redzoo/math/display.py
@@ -3,15 +3,16 @@ from datetime import timedelta
 
 
 
-def duration(elapsed, digits: int = 0):
-    if isinstance(elapsed, timedelta):
-        elapsed = elapsed.total_seconds()
+def duration(elapsed_seconds, digits: int = 0):
+    if isinstance(elapsed_seconds, timedelta):
+        elapsed_seconds = elapsed_seconds.total_seconds()
 
-    if elapsed > 60 * 60:
-        hours = round(elapsed/(60*60), digits)
+    if elapsed_seconds > 60 * 60:
+        hours = round(elapsed_seconds / (60 * 60), digits)
         return str(hours if digits>0 else int(hours)) + " hour"
-    elif elapsed > 60:
-        minutes = round(elapsed/60, digits)
+    elif elapsed_seconds > 60:
+        minutes = round(elapsed_seconds / 60, digits)
         return str(minutes if digits>0 else int(minutes)) + " min"
     else:
-        return str(elapsed if digits>0 else int(elapsed)) + " sec"
+        seconds = round(elapsed_seconds, digits)
+        return str(seconds if digits>0 else int(seconds)) + " sec"
